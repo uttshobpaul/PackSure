d06548134663d9e6a08cd6d1b9bea181194893c5
UTTSHOB PAUL <uttshobpaul20@gmail.com>
2026-09-05 11:28:24 +0530

    Configure static files for Render

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 # =========================
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # =========================
 # MEDIA / UPLOADED IMAGES
